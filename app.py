0285a4c9fd2b95b1a30dee90a7cddcc8694fad57
Jack Weinbender <jack.weinbender@gmail.com>
2019-02-18 08:39:43 -0600

    Boilerplate

--- app.py
+++ app.py
@@ -1,17 +1,53 @@
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+
+
+###
+# Routing for your application.
+###
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
+def home():
+    """Render website's home page."""
     return """
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
     <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    """
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
