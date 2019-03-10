c5b79a4d6a5a1cbf8d21833272c5192ac53c669e
Jack Weinbender <jack.weinbender@gmail.com>
2019-03-10 17:37:08 -0500

    Sign tests

---
+++ test/sign_tests.py
@@ -0,0 +1,20 @@
+# Tests for the App
+from models.sign import Sign
+import json
+
+class TestSign:
+    def test_from_sheets_row_builds_sign(self):
+        """Asserts that the sheet-cache serializes to what we have in the signlist"""
+        with open('test/data/sheets-cache.json') as f:
+            data = json.load(f)
+
+        with open('data/signlist.json') as zl:
+            signlist = json.load(zl)
+
+        for row in data:
+            sign = Sign.from_sheets_row(row)
+            expected = signlist[sign.borger_id]
+            assert sign.__dict__ == expected
+            
+            
+        
