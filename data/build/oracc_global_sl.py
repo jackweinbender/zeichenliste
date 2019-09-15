8245cf4857a0be36ead6f856a2ef9a6972251e3e
Oeyvind Bjoeru <oeyvind.bjoeru@gmail.com>
2019-09-15 09:41:49 -0500

    Typos and fix
    Now it works

--- data/build/oracc_global_sl.py
+++ data/build/oracc_global_sl.py
@@ -10,12 +10,14 @@ possible_keys = ['gdl', 'utf8', 'hex', 'values', 'uphase',
 values_to_signs = {}    # dictionary of every "value" :: "sign"
 sign_to_values = {}     # dictionary of every "sign" :: ["value1", "value2"...]
 
-def get_signs_from_json()
+
+def get_signs_from_json():
     path = os.path.dirname(os.path.dirname( __file__ )) + '/oracc_global_sl.json'
     with open(path) as signs_file:
         json_signs = json.load(signs_file)
+    return json_signs
 
-def populate_dicts()
+def populate_dicts(json_signs):
     for value in json_signs['index']:
         sign = json_signs['index'][value]
         values_to_signs[value] = json_signs['index'][value]
@@ -24,5 +26,4 @@ def populate_dicts()
         else:
             sign_to_values[sign] = [value]
 
-get_signs_from_json()
-populate_dicts()
+populate_dicts(get_signs_from_json())
