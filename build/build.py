55a57a6fbdf4fa499993c67ff7374759b6079ff6
Oeyvind Bjoeru <oeyvind.bjoeru@gmail.com>
2019-09-23 10:27:04 -0500

    Pulls ogsl
    Pulls ogsl from oracc on GitHub
    Backup file if needed

--- build/build.py
+++ build/build.py
@@ -2,7 +2,7 @@
 # Google Docs and generating the data files that 
 # we use to power this thing.
 
-import requests, json
+import requests, json, os
 from collections import defaultdict
 from models.sign import Sign
 from models.sign_value import SignValue
@@ -16,67 +16,93 @@ def cleanup(d):
 
     return d
 
-# Spreadsheet details
-SPREADSHEET_ID = '1H3OsWtoybznhHtFzSxamPzEvCoQGOhM8sJXYOrNtsgw'
-URL = f'https://spreadsheets.google.com/feeds/list/{SPREADSHEET_ID}/od6/public/values?alt=json'
+def get_spreadsheet():
+    # Spreadsheet details
+    SPREADSHEET_ID = '1H3OsWtoybznhHtFzSxamPzEvCoQGOhM8sJXYOrNtsgw'
+    URL = f'https://spreadsheets.google.com/feeds/list/{SPREADSHEET_ID}/od6/public/values?alt=json'
 
-# Get the data
-data = requests.get(URL).json()
-# Drill down to the stuff we care about (Rows is a LIST)
-rows = data['feed']['entry']
+    # Get the data
+    data = requests.get(URL).json()
+    # Drill down to the stuff we care about (Rows is a LIST)
+    rows = data['feed']['entry']
 
-# Dump the latest sheet into the test data so we know we don't break anything
-with open('test/data/sheets-cache.json', 'w') as outfile:
-    json.dump(rows, outfile, ensure_ascii=False, sort_keys=True, indent=4)
+    # Dump the latest sheet into the test data so we know we don't break anything
+    with open('test/data/sheets-cache.json', 'w') as outfile:
+        json.dump(rows, outfile, ensure_ascii=False, sort_keys=True, indent=4)
 
-# The output SIGNLIST
-signlist = {}
-index = defaultdict(list)
+    # The output SIGNLIST
+    signlist = {}
+    index = defaultdict(list)
 
-for row in rows:
-    # Gets the Borger number for the KEY
-    borger = row['gsx$borger']['$t']
+    for row in rows:
+        # Gets the Borger number for the KEY
+        borger = row['gsx$borger']['$t']
 
-    # Serialize the row as a Sign Object
-    sign = Sign.from_sheets_row(row)
-    
-    # Add Sign to signlist (as dict, for later JSON output)
-    signlist[borger] = sign.__dict__
+        # Serialize the row as a Sign Object
+        sign = Sign.from_sheets_row(row)
 
-    # Add Borger_id to all index values
-    indexed_keys = [
-        'borger_id', 
-        'labat_id', 
-        'huehnergard_id', 
-        'deimel_id', 
-        'mittermayer_id', 
-        'hethzl_id',
-        # 'labat_name',
-        # 'borger_name', 
-        # 'oracc_name',
-        # 'unicode_name',
-    ]
+        # Add Sign to signlist (as dict, for later JSON output)
+        signlist[borger] = sign.__dict__
 
-    # Make the index for signlist numbers
-    for k in indexed_keys:
-        # Get property value by name
-        val = getattr(sign,k)
-        idx = val.lower()
-        # Add Borger ID to index
-        if val != '' and sign.borger_id not in index[idx]:
-            index[idx].append(sign.borger_id)
+        # Add Borger_id to all index values
+        indexed_keys = [
+            'borger_id',
+            'labat_id',
+            'huehnergard_id',
+            'deimel_id',
+            'mittermayer_id',
+            'hethzl_id',
+            # 'labat_name',
+            # 'borger_name',
+            # 'oracc_name',
+            # 'unicode_name',
+        ]
 
-    for value in sign.values:
-        vs = SignValue(value).get_keys()
-        for v in vs:
-            idx = Normalizer.normalize(v)
-            if sign.borger_id not in index[idx]:
+        # Make the index for signlist numbers
+        for k in indexed_keys:
+            # Get property value by name
+            val = getattr(sign,k)
+            idx = val.lower()
+            # Add Borger ID to index
+            if val != '' and sign.borger_id not in index[idx]:
                 index[idx].append(sign.borger_id)
+
+        for value in sign.values:
+            vs = SignValue(value).get_keys()
+            for v in vs:
+                idx = Normalizer.normalize(v)
+                if sign.borger_id not in index[idx]:
+                    index[idx].append(sign.borger_id)
         
+def get_ogsl():
+
+    with open('data/signlist.json', 'w') as outfile:
+        json.dump(signlist, outfile, ensure_ascii=False, sort_keys=True, indent=4)
+
+    with open('data/search_index.json', 'w') as outfile:
+        json.dump(index, outfile, ensure_ascii=False, sort_keys=True, indent=4)
+
+#https://github.com/oracc/ogsl/blob/master/00lib/ogsl.asl
+
+def pull_ogsl():
+
+    # Pull ogsl from Oracc on GitHub
+    response = requests.get('https://raw.githubusercontent.com/oracc/ogsl/master/00lib/ogsl.asl')
+    ogsl = response.text.splitlines()
+
+    # If the url doesn't work, here is the backup as of 9/23/2019
+    #path = os.path.dirname(os.path.dirname(__file__)) + '/data/ogsl.txt'
+    #with open(path) as file: ogsl = file.readlines()
+
+    possible_keys = list()
+
+    for line in ogsl:
+        key = line[0:line.expandtabs().find(' ')].strip()
+        value = line[line.expandtabs().find(' '):].strip()
+        if key != '': possible_keys.append(key.strip())
+
+    for k in set(possible_keys):
+        print(k, '\t', possible_keys.count(k))
 
 
-with open('data/signlist.json', 'w') as outfile:
-    json.dump(signlist, outfile, ensure_ascii=False, sort_keys=True, indent=4)
-
-with open('data/search_index.json', 'w') as outfile:
-    json.dump(index, outfile, ensure_ascii=False, sort_keys=True, indent=4)
+pull_ogsl()
